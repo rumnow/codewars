095c2dec52b227b7b30e0e189ffdae25a78ce90a
Roman Ushakov <goose_efim@icloud.com>
2024-01-28 16:59:50 +0100

    hm

--- 24-01-27.go
+++ 24-01-27.go
@@ -1,23 +1,17 @@
 package main
 
-import (
-	"slices"
-)
-
 //https://leetcode.com/problems/count-elements-with-maximum-frequency/
 func maxFrequencyElements(nums []int) (res int) {
-	slices.Sort(nums)
-	seq := false
-	for i := 1; i < len(nums); i++ {
-		if nums[i] == nums[i-1] {
-			if seq {
-				res++
-			} else {
-				res+=2
-			}
-			seq = true
-		} else {
-			seq = false
+	type frequency map[int]int
+	freq := frequency{}
+	for _, v := range nums {
+		freq[v] += 1
+	}
+	maxV, maxK := 0, 0
+	for k, v := range freq {
+		if v >= maxV && k >= maxK {
+			maxK, maxV = k, v
+			res = k * v
 		}
 	}
 	return
--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 func main() {
 	fmt.Println(4, maxFrequencyElements([]int{4,2,3,5,7,4,2}))
 	fmt.Println(7, maxFrequencyElements([]int{4,2,3,5,7,4,2,2,7}))
-	fmt.Println(0, maxFrequencyElements([]int{1,2,3,4,5}))
+	fmt.Println(5, maxFrequencyElements([]int{1,2,3,4,5}))
+	fmt.Println(4, maxFrequencyElements([]int{1,2,2,3,1,4}))
+	fmt.Println(4, maxFrequencyElements([]int{1,2,2,3,1,4}))
 	fmt.Println(findLongestChain([][]int{{4,3},{1,2},{5,6},{7,8}}))
 	fmt.Println(minimumPushes2("xyzxyzxyzxyz"))
 	fmt.Println(minimumPushes("xycdefghij"))
